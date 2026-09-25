8eb5300dae9a0cc2b9897384c3a8051b6868678f
germanpiasco <74624567+germanpiasco@users.noreply.github.com>
2026-09-25 12:03:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='proyecto-thor-2.1.0';
+const CACHE='proyecto-thor-2.2.0';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
